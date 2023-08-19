d33a4c46d1b1352818070d86f4f03a983d6e1da0
KevinYeff <kevinyeff94@gmail.com>
2023-08-18 22:35:22 -0500

    feat: Initializing main class and  public instances were added to the main class

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-#contributor: Kevin Espinoza Salguedo
+# contributor: Kevin Espinoza Salguedo
+
+from uuid import uuid4
+from datetime import datetime
 
 """Main/base Class for the project"""
+
 
 class BaseModel():
     """This class will define all the common
     attributes/methods for other classes"""
-    #Public instance attributes
-    def __init__(self, id= "", created_at=None, updated_at=None):
-        pass
+    # Public instance attributes
+
+    def __init__(self, id="", created_at=None, updated_at=None):
+        """Initializing the class with the following 
+        public attributes"""
+
+        id = str(uuid4())
+        created_at = datetime.now()
+        updated_at = datetime.now()
