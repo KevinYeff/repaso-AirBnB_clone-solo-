ebdef4e2363ff64fecf2aec16fbb6e5e106693f6
KevinYeff <kevinyeff94@gmail.com>
2023-08-18 22:25:16 -0500

    feat: models dir and main class base_model BaseModel created

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#contributor: Kevin Espinoza Salguedo
+
+"""Main/base Class for the project"""
+
+class BaseModel():
+    """This class will define all the common
+    attributes/methods for other classes"""
+    #Public instance attributes
+    def __init__(self, id= "", created_at=None, updated_at=None):
+        pass
