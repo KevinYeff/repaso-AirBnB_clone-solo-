f6df72a1e1ff59ac6e1f43d47ae0f187e31a6ee3
KevinYeff <kevinyeff94@gmail.com>
2023-08-22 11:45:58 -0500

    update: dictionary named classes and the call of the reaload method in variable storage added The dictionary classes will contain the base class name that will help us to instance new objects from the reload method inside the FileStorage class we also added the import of the BaseModelClass and  a call reload() for the variable storage to make sure that this surely method works

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 from .engine.file_storage import FileStorage
+from .base_model import BaseModel
+classes = {
+    "BaseModel": BaseModel
+}
 
 storage = FileStorage()
+storage.reload()
