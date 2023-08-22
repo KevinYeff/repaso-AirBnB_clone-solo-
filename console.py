997d22b963176a47476a7aac4c3c7a0ca25d2bdd
KevinYeff <kevinyeff94@gmail.com>
2023-08-22 15:53:27 -0500

    update: New method for the command line interpreted named create added This method will create a new instance of BaseModel EX: create BaseModel -> creates a new instance create   -> class name is missing create randomClass -> class doesn't exist

--- console.py
+++ console.py
@@ -1,31 +1,48 @@
 #!/usr/bin/python3
-#Contributor: Kevin Yeff Espinoza Salguedo
+# Contributor: Kevin Yeff Espinoza Salguedo
 
 """Program console 0.0.1 that uses the cmd module"""
 import cmd
+import models
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
-    
-    """Class that will be the entry point of the command interpreter""" 
 
-    prompt = "(hbnb )"
-    
+    """Class that will be the entry point of the command interpreter"""
+
+    prompt = "(hbnb) "
+
     def do_quit(self, arg):
         """Method that exits the program"""
         return (True)
-    
+
     def do_EOF(self, arg):
         """Secures a clean exit"""
         return True
-    
+
     def do_help(self, arg: str):
         """Method that gives the user a short description of the 
         commands"""
         return super().do_help(arg)
-    
+
     def emptyline(self):
         pass
-    
+    # New commands
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it in to the 
+        JSON file and also prints the id of the instance"""
+        # parameters = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif arg not in models.classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-    
