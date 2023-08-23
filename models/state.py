8a56f9c4ac70f9db62da169e2e4863c553fbc764
KevinYeff <kevinyeff94@gmail.com>
2023-08-22 23:08:17 -0500

    feat: new class named state added This class inherits from main/base BaseModel() class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# contributor: Kevin Yeff Espinoza Salguedo
+
+"""New class State"""
+
+from .base_model import BaseModel
+
+
+class State(BaseModel):
+    """CLass that inherits from the main/base class
+    BaseModel() and has the following public attributes"""
+
+    name = ""
