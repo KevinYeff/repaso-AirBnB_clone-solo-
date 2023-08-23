ebb2d4e0ec10ba41d9aa979c13f8a6fdb5dc56d5
KevinYeff <kevinyeff94@gmail.com>
2023-08-22 23:11:21 -0500

    update: reference to State() class added

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,12 @@
 from .engine.file_storage import FileStorage
 from .base_model import BaseModel
 from .user import User
+from .state import State
 
 classes = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "State": State
 }
 
 storage = FileStorage()
