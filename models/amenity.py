2ec19d6c7bb85ca37d3eebb5ea5d71cd2ac1b1ba
KevinYeff <kevinyeff94@gmail.com>
2023-08-22 23:19:43 -0500

    feat: New class named Amenity() added This class inherits from main/base BaseModel() class

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# contributor: Kevin Yeff Espinoza Salguedo
+
+"""New class Amenity"""
+
+from .base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class that inherits from main/base
+    BaseModel() class and has the following
+    public class attribute"""
+
+    name = ""
