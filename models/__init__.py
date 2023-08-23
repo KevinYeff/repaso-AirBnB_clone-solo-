ba6a9aef8ac9b4f9332a27b7313ea4aa7df50564
KevinYeff <kevinyeff94@gmail.com>
2023-08-22 23:17:05 -0500

    feat: Reference to City() class added

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,13 @@ from .engine.file_storage import FileStorage
 from .base_model import BaseModel
 from .user import User
 from .state import State
+from .city import City
 
 classes = {
     "BaseModel": BaseModel,
     "User": User,
-    "State": State
+    "State": State,
+    "City": City
 }
 
 storage = FileStorage()
