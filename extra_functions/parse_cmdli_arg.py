9fe775311099982572efa4c7a99056172387d6af
KevinYeff <kevinyeff94@gmail.com>
2024-04-01 04:38:47 -0500

    update: Now int_or_float_or_string() method handles negative numbers

--- extra_functions/parse_cmdli_arg.py
+++ extra_functions/parse_cmdli_arg.py
@@ -1,19 +1,28 @@
-"""Function to parse a cmdli argument ver 0.0.1"""
+#!/usr/bin/python3
+# Contributor: Kevin Yeff Espinoza Salguedo
+
+"""Function to parse a cmdli argument ver 0.0.2"""
 
 
 def int_or_float_or_string(arg):
     """
-    Function to parse a cmdli argument ver 0.0.1
+    Function to parse a cmdli argument ver 0.0.2
     :param arg: cmdli argument
     :return: parsed argument with the correct type value
     """
     if isinstance(arg, str):
         if arg.isdigit():
             return int(arg)
+        elif arg.startswith('-') and "." not in arg:
+            return int(arg)
         else:
-            if "." in arg:
-                parameters = arg.split(".")
-                if parameters[0].isdigit() and parameters[1].isdigit():
-                    return float(arg)
-                else:
-                    return arg
+            parameters = arg.split(".")
+            if parameters[0].isdigit() and parameters[1].isdigit():
+                return float(arg)
+            elif parameters[0].startswith("-") and parameters[1].isdigit():
+                return float(arg)
+            else:
+                # if arg is not int or float return the arg
+                return arg.strip('"')
+    # return the arg without any changes
+    return arg
