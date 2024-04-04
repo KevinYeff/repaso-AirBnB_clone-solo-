c176dc4e7f3cfbfcb72e9a53db67e2e75adaf480
KevinYeff <kevinyeff94@gmail.com>
2024-04-03 20:25:54 -0500

    doc: comments to explain code added

--- extra_functions/extract_id.py
+++ extra_functions/extract_id.py
@@ -13,10 +13,15 @@ def id_parser(arg):
     :inital example input: "246c227a-d5c1-403d-9bc7-6a47bb9f0f68")
     """
     if arg:
+        # define regex
         pattern = r'("([^"*\,\s{}]*)")((?:\)))'
+        # if there are coincidences
         coincidences = re.match(pattern, arg)
         if coincidences:
+            # extract the id
             coincidence = coincidences[2]
+            # return the string cotaining the id
             return coincidence
     else:
+        # otherwise do nothing
         return None
